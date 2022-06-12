40b6fe1fbb4f740145aa0e9ee448779c4bc03b49
maria vera <asuvera93@gmail.com>
2022-06-12 01:28:23 -0400

    Componente counter

---
+++ counter-app/src/Component/Counter.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useState } from 'react';
+
+const Counter =()=> {
+    const [num, setNum]= useState(0);
+    const addNum = ()=>{setNum(num <= 19 ? num + 1: 20)};
+    const restNum =()=>{setNum(num >= 1 ? num - 1: 0)};
+
+    return (
+
+        <div>
+            <p className='counterName'>Counter 1</p>
+            <button className='add' onClick={addNum}>+</button>
+            <p className='num'>{num}</p>
+            <button className='rest'onClick={restNum}>-</button>
+        </div>
+        
+    )
+
+
+}
+export default Counter
